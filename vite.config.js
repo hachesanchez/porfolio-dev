027d089d7a35d60ba215242039a51c5dbfbad58c
Helena Sánchez <helena.sangar@gmail.com>
2023-11-21 10:52:13 +0100

    config updated images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ViteImagesPlugin from 'vite-plugin-images';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ViteImagesPlugin()],
+});
