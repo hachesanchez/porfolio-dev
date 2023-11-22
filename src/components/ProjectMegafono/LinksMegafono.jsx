5a940dede8d57f31bd2f993b141f74e6674f89cb
Helena Sánchez <helena.sangar@gmail.com>
2023-11-22 12:15:59 +0100

    logo3, megafono credentials

--- src/components/ProjectMegafono/LinksMegafono.jsx
+++ src/components/ProjectMegafono/LinksMegafono.jsx
@@ -1,60 +1,51 @@
-import './LinksMegafono.css'
-import { Container } from 'react-bootstrap'
+import "./LinksMegafono.css";
+import { Container } from "react-bootstrap";
 
-const LinksMegafono = ({ repository, link }) => {
-
-    return (
-
-        <div className="megafono-links-component">
-            <Container >
-
-                <div className='links-container' >
-                    <div className="megafono-web">
-                        <a
-                            className="megafono-weblink"
-                            href={link}
-                            target='blank'
-                            rel="noopener noreferrer"
-                        >
-                            {link}
-                        </a>
-                    </div>
-                    <div className="megafono-repo">
-                        <p className='megafono-repo-title'>
-                            Repository:
-                        </p>
-                        <ul>
-                            <li>
-                                Client:
-                                <a
-                                    className="megafono-webrepo"
-                                    href={repository.client}
-                                    target='blank'
-                                    rel="noopener noreferrer"
-                                >
-                                    {repository.client}
-                                </a>
-                            </li>
-                            <li>
-                                Server:
-                                <a
-                                    className="megafono-webrepo"
-                                    href={repository.server}
-                                    target='blank'
-                                    rel="noopener noreferrer"
-                                >
-                                    {repository.server}
-                                </a>
-                            </li>
-
-                        </ul>
-                    </div >
-                </div>
-
-            </Container >
+const LinksMegafono = ({ repository, link: { dns, credentials } }) => {
+  return (
+    <div className="megafono-links-component">
+      <Container>
+        <div className="links-container">
+          <div className="megafono-web">
+            <a
+              className="megafono-weblink"
+              href={dns}
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              {dns}
+            </a>
+            <p className="megafono-credentials">{credentials}</p>
+          </div>
+          <div className="megafono-repo">
+            <p className="megafono-repo-title">Repository:</p>
+            <ul>
+              <li>
+                <a
+                  className="megafono-webrepo"
+                  href={repository.client}
+                  target="blank"
+                  rel="noopener noreferrer"
+                >
+                  Client
+                </a>
+              </li>
+              <li>
+                <a
+                  className="megafono-webrepo"
+                  href={repository.server}
+                  target="blank"
+                  rel="noopener noreferrer"
+                >
+                  Server
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </Container>
+    </div>
+  );
+};
 
-    )
-}
-
-export default LinksMegafono
+export default LinksMegafono;
